28ca8dffaa328bb8aa08043d55d1a0a89055f502
Dan Jacobellis <danjacobellis@utexas.edu>
2021-07-13 20:51:24 -0500

    resources to build

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+project = u'445S Lab Manual'
+copyright = u'2021'
+source_suffix = ['.rst', '.md']
+master_doc = 'lab'
+language = None
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+pygments_style = 'sphinx'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+htmlhelp_basename = 'RTDSphinxThemeSampledoc'
+extensions = ['myst_parser']
